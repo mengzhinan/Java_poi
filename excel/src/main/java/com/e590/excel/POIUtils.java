52516582096a77561c1cf4bb26e753e7b2900070
duke <136239173@qq.com>
2021-10-17 18:35:54 +0800

    init

--- excel/src/main/java/com/e590/excel/POIUtils.java
+++ excel/src/main/java/com/e590/excel/POIUtils.java
@@ -6,8 +6,10 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * author: duke
@@ -16,28 +18,106 @@ import java.io.FileOutputStream;
  */
 public class POIUtils {
 
+    private static boolean isEmpty(String txt) {
+        return txt == null || txt.trim().length() == 0;
+    }
+
+    /**
+     * 创建文件和目录，最后判断文件是否存在
+     *
+     * @param filePath
+     * @return
+     */
+    private static boolean createFileIfNotExists(String filePath) {
+        if (isEmpty(filePath)) {
+            return false;
+        }
+        File file = new File(filePath);
+        File parent = file.getParentFile();
+        if (!parent.exists()) {
+            parent.mkdirs();
+        }
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return file.exists();
+    }
+
     public static void readTxt(String txtFile) {
 
     }
 
-    public static void writeExcel(String excelFile, String txt) {
+    /**
+     * 写入 Excel 文件
+     *
+     * @param excelFile excel 文件路径 + 文件名
+     * @param sheetName sheet 表名
+     * @param dateSet   二维结构的数据集合
+     */
+    public static void writeExcel(String excelFile, String sheetName, ArrayList<ArrayList<String>> dateSet)
+            throws IOException, IllegalArgumentException {
+        if (isEmpty(sheetName)) {
+            sheetName = "sheet1";
+        }
+        boolean isXlsFileExists = createFileIfNotExists(excelFile);
+        if (!isXlsFileExists) {
+            throw new IllegalArgumentException(excelFile + " not exists Exception.");
+        }
+        FileOutputStream fos = null;
+        Workbook workbook = null;
         try {
+            fos = new FileOutputStream(excelFile);
             //1.创建workbook
-            Workbook workbook=new HSSFWorkbook();
+            workbook = new HSSFWorkbook();
             //2.根据workbook创建sheet
-            Sheet sheet = workbook.createSheet("会员列表");
+            Sheet sheet = workbook.createSheet(sheetName);
+
             //3.根据sheet创建row
-            Row row1 = sheet.createRow(0);
-            //4.根据row创建cell
-            Cell cell1 = row1.createCell(0);
-            //5.向cell里面设置值
-            cell1.setCellValue("按键");
+            if (dateSet != null && dateSet.size() > 0) {
+                //循环行
+                for (int line = 0; line < dateSet.size(); line++) {
+                    Row row1 = sheet.createRow(line);
+                    // 循环列
+                    ArrayList<String> columnList = dateSet.get(line);
+                    if (columnList != null && columnList.size() > 0) {
+                        for (int cell = 0; cell < columnList.size(); cell++) {
+                            //4.根据row创建cell
+                            Cell cell1 = row1.createCell(cell);
+                            //5.向cell里面设置值
+                            cell1.setCellValue(columnList.get(cell));
+                        }
+                    }
+                }
+            }
             //6.通过输出流写到文件里去
-            FileOutputStream fos=new FileOutputStream("D:\\123\\01.xls");
             workbook.write(fos);
-            fos.close();
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
+            throw new IOException(e);
+        } finally {
+            if (workbook != null) {
+                try {
+                    workbook.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (fos != null) {
+                try {
+                    fos.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
--- excel/src/main/java/com/e590/excel/Test.java
+++ excel/src/main/java/com/e590/excel/Test.java
@@ -1,5 +1,7 @@
 package com.e590.excel;
 
+import java.util.ArrayList;
+
 /**
  * author: duke
  * date_time: 2021-10-17 17:41:53
@@ -7,9 +9,33 @@ package com.e590.excel;
  */
 public class Test {
 
-    public static void main(String[] args){
-        POIUtils.writeExcel(null,null);
-        System.out.println("write excel finish");
+    public static void main(String[] args) {
+
+        POIUtils.writeExcel("C:\\Users\\duke\\Desktop\\target.xls", null, makeList());
+        System.out.println("写入 Excel 文件完成");
+    }
+
+    private static ArrayList<ArrayList<String>> makeList() {
+        ArrayList<ArrayList<String>> dateSet = new ArrayList<>();
+
+        ArrayList<String> row1 = new ArrayList<>();
+        row1.add("表头1");
+        row1.add("表头2");
+        row1.add("表头3");
+        ArrayList<String> row2 = new ArrayList<>();
+        row2.add("内容1");
+        row2.add("内容2");
+        row2.add("内容3");
+        ArrayList<String> row3 = new ArrayList<>();
+        row3.add("内容11");
+        row3.add("内容22");
+        row3.add("内容33");
+        dateSet.add(row1);
+        dateSet.add(row2);
+        dateSet.add(row3);
+
+
+        return dateSet;
     }
 
 }
