39fbf83e34c0ffe00b138dd9483e735366c517dc
duke <136239173@qq.com>
2021-10-17 21:27:17 +0800

    init

--- excel/src/main/java/com/e590/excel/Test.java
+++ excel/src/main/java/com/e590/excel/Test.java
@@ -1,6 +1,5 @@
 package com.e590.excel;
 
-import java.io.IOException;
 import java.util.ArrayList;
 
 /**
@@ -11,26 +10,17 @@ import java.util.ArrayList;
 public class Test {
 
     public static void main(String[] args) {
-
         try {
-
-            ArrayList<String> date = ExcelUtils.readTxt("C:\\Users\\duke\\Desktop\\a.txt");
-
-
-            ExcelUtils.writeExcel("C:\\Users\\duke\\Desktop\\target.xls", "文件分析", makeList());
+            ArrayList<String> data = ExcelUtils.readTxt("C:\\Users\\duke\\Desktop\\a.txt");
+            if (data == null || data.size() == 0) {
+                throw new IllegalArgumentException("data is null or empty, please check origin data file format.");
+            }
+            ExcelUtils.writeExcel("C:\\Users\\duke\\Desktop\\target.xls", "文件分析", data);
             System.out.println("写入 Excel 文件成功");
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
             System.out.println("写入 Excel 文件失败");
         }
-    }
-
-    private static ArrayList<String> makeList() {
-        ArrayList<String> list = new ArrayList<>();
-        list.add("表头1,表头2,表头3");
-        list.add("内容1,内容2,内容3");
-        list.add("内容11,内容22,内容33");
-        return list;
     }
 
 }
